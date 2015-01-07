094ad2217e921f57746b26dc510a4206334c7e50
Cyberarm <matthewlikesrobots@gmail.com>
2015-01-07 13:30:36 -0600

    Added Bullet, removed played_with_id from PlayedWith.

--- db/migrate/20141206151000_create_played_withs.rb
+++ db/migrate/20141206151000_create_played_withs.rb
@@ -1,9 +1,8 @@
 class CreatePlayedWiths < ActiveRecord::Migration
   def change
     create_table :played_withs do |t|
-      t.integer :player_id, required: true
+      t.integer :player_id, required: true # Person Player played with, not Player.
       t.integer :activity_id, required: true
-      t.integer :played_with_id, required: true
 
       t.timestamps
     end
